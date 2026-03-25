8eb4d78184fd5cf912d6f0b57289fbb631d87bf2
Ritchell-Malang <202310885@gordoncollege.edu.ph>
2026-03-26 01:10:47 +0800

    change ccs to tailwind, create useNavigate, create register and HomePage

--- digital_wallet/src/App.js
+++ digital_wallet/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+//import "./App.css";
 import LoginPage from "./components/LoginPage";
+import Register from "./components/RegisterPage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
